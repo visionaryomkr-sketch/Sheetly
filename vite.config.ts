e70171abea446cdb7360b1f8e5dc387d19890e59
visionaryomkr-sketch <visionaryomkr@gmail.com>
2026-05-18 12:36:06 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,62 @@
+// import tailwindcss from '@tailwindcss/vite';
+// import react from '@vitejs/plugin-react';
+// import path from 'path';
+// import { defineConfig, loadEnv } from 'vite';
+
+// export default defineConfig(({ mode }) => {
+//   const env = loadEnv(mode, '.', '');
+//   return {
+//     plugins: [react(), tailwindcss()],
+//     build: {
+//       outDir: 'dist',
+//     },
+//     define: {
+//       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+//     },
+//     resolve: {
+//       alias: {
+//         '@': path.resolve(__dirname, '.'),
+//       },
+//     },
+//     server: {
+//       hmr: process.env.DISABLE_HMR !== 'true',
+//       watch: process.env.DISABLE_HMR === 'true' ? null : {},
+//     },
+//   };
+// });
+
+
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 
+import { cloudflare } from "@cloudflare/vite-plugin";
+
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
+
   return {
-    plugins: [react(), tailwindcss()],
+    plugins: [react(), tailwindcss(), cloudflare()],
+    
+    // Yeh dono lines add kar do
+    publicDir: 'public',
+    
     build: {
       outDir: 'dist',
+      assetsDir: 'assets',
     },
+
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
     },
+
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
       },
     },
+
     server: {
       hmr: process.env.DISABLE_HMR !== 'true',
       watch: process.env.DISABLE_HMR === 'true' ? null : {},
