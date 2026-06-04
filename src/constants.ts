8edc21e66c0aa503b18b1125517750168da0cd56
visionaryomkr-sketch <visionaryomkr@gmail.com>
2026-06-04 19:13:10 +0530

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
 export const PAYMENT_URL = "https://superprofile.bio/vp/tj9_xpSO";
-export const PRODUCT_PRICE = "₹499";
-export const ORIGINAL_PRICE = "₹12,496";
+export const PRODUCT_PRICE = "₹899";
+export const ORIGINAL_PRICE = "₹4,496";
 export const SAVINGS = "₹11,197";
